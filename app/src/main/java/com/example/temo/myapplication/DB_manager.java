00730a4bd47d8be1425b1b183d5c5c96757a18eb
temo <hatemragap5@gmail.com>
2018-07-30 16:11:37 -0700

    kkjhed

--- app/src/main/java/com/example/temo/myapplication/DB_manager.java
+++ app/src/main/java/com/example/temo/myapplication/DB_manager.java
@@ -17,10 +17,6 @@ class DB_manager extends SQLiteOpenHelper {
     private static final String COST = "cost";
     private static final String METHOD = "method";
     private static final String TABLE_NAME = "tabe";
-    public static final String TABLE_DETELS = "tabedetels";
-    public static final String DETELS_NEWCOST = "newcost";
-    public static final String DETELS_DAT = "newdate";
-    public static final String DETELS_ID = "newid";
 
     private Context context;
 
@@ -36,25 +32,13 @@ class DB_manager extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
 
         db.execSQL("CREATE TABLE " + TABLE_NAME + "( " + ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + NAME + " TEXT," + PHONE + " TEXT," + DATE + " TEXT ," + COST + " TEXT ," + METHOD + " TEXT ); ");
-        //  db.execSQL("CREATE table " + TABLE_DETELS + "( " + DETELS_ID + " INTEGER," + DETELS_NEWCOST + " TEXT," + DETELS_DAT + " TEXT);");
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
-
-    /*public int getId2() {
-        String com[] = new String[]{DETELS_ID};
-        Cursor cursor = sqLiteDatabase.query(TABLE_DETELS, com, null, null, null, null, null);
-        int idd = 100;
-
-
-        idd = cursor.getInt(cursor.getColumnIndex(DETELS_ID));
-
-
-        return idd;
-    }*/
 
     public void insert(String name, String phone, String date, String cost, String method) {
         ContentValues contentValues = new ContentValues();
@@ -63,7 +47,7 @@ class DB_manager extends SQLiteOpenHelper {
         contentValues.put(DATE, date);
         contentValues.put(COST, cost);
         contentValues.put(METHOD, method);
-       sqLiteDatabase.insert(TABLE_NAME, null, contentValues);
+        sqLiteDatabase.insert(TABLE_NAME, null, contentValues);
 
     }
 
@@ -76,7 +60,7 @@ class DB_manager extends SQLiteOpenHelper {
         while (cursor.moveToNext()) {
             arrayList.add(new Mitem(cursor.getInt(cursor.getColumnIndex(ID)), cursor.getString(cursor.getColumnIndex(NAME)), cursor.getString(cursor.getColumnIndex(PHONE)), cursor.getString(cursor.getColumnIndex(DATE)), cursor.getString(cursor.getColumnIndex(COST)), cursor.getString(cursor.getColumnIndex(METHOD))));
         }
-
+        cursor.close();
 
         return arrayList;
     }
